a85915eebc72be545541fa2d606b8c9d64d8746b
kimbychan <kimuc2121@gmail.com>
2023-10-13 01:01:49 +0900

    장고오류

--- pt/lib/main.dart
+++ pt/lib/main.dart
@@ -1,20 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          title: Text('HTTP Request Example'),
+        ),
         body: Center(
-          child: Text('Hello World!'),
+          child: ElevatedButton(
+            onPressed: () {
+              fetchData();
+            },
+            child: Text('Fetch Data'),
+          ),
         ),
       ),
     );
+  }
+}
+
+Future<void> fetchData() async {
+  final response = await http.get(Uri.parse('https://example.com/api/data/'));
+  if (response.statusCode == 200) {
+    print('데이터: ${response.body}');
+  } else {
+    print('오류: ${response.statusCode}');
   }
 }
