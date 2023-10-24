92762afadc6a951d4bf57b6321733f5db4a5c95d
kimbychan <kimuc2121@gmail.com>
2023-10-24 16:20:26 +0900

    asd

--- lib/login.dart
+++ lib/login.dart
@@ -1,46 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
-  void _handleLogin() {
+  Future<void> _handleLogin() async {
     String username = _usernameController.text;
     String password = _passwordController.text;
 
-    // TODO: Handle the login logic for the selected login option
-    // You can add your authentication logic here
+    final url = Uri.parse('http://127.0.0.1:8000/accounts/login/');
+    // Django API 엔드포인트를 여기에 입력
 
-    // Example: Check if both username and password are not empty
-    if (username.isNotEmpty && password.isNotEmpty) {
-      // Successful login, you can navigate to the main screen or perform other actions
-      // Navigator.of(context).push(MaterialPageRoute(
-      //   builder: (context) => MainScreen(),
-      // ));
-      print('Logged in with Username: $username and Password: $password');
+    final response = await http.post(
+      url,
+      body: {
+        'username': username,
+        'password': password,
+      },
+    );
+    print('Response Status Code: ${response.statusCode}');
+    print('Response Body: ${response.body}');
+
+    if (response.statusCode == 200) {
+      final jsonResponse = json.decode(response.body);
+      final message = jsonResponse['message'];
+
+      if (message == 'Login successful') {
+        // 로그인 성공 처리
+        print('Logged in with Username: $username and Password: $password');
+      } else {
+        // 로그인 실패 처리
+        print('Login failed. Please enter valid credentials.');
+      }
     } else {
-      // Show an error message or perform other actions for unsuccessful login
-      print('Login failed. Please enter both username and password.');
-    }
-  }
-
-  void _handleRegistration() {
-    String username = _usernameController.text;
-    String password = _passwordController.text;
-
-    // TODO: Handle the registration logic
-    // You can add your registration logic here
-
-    // Example: Check if both username and password are not empty
-    if (username.isNotEmpty && password.isNotEmpty) {
-      // Successful registration, you can navigate to the main screen or perform other actions
-      // Navigator.of(context).push(MaterialPageRoute(
-      //   builder: (context) => MainScreen(),
-      // ));
-      print('Registered with Username: $username and Password: $password');
-    } else {
-      // Show an error message or perform other actions for unsuccessful registration
-      print('Registration failed. Please enter both username and password.');
+      // 서버로부터 200 상태 코드가 아닌 응답을 받았을 때의 처리
+      print('Login request failed. Please try again.');
     }
   }
 
@@ -56,27 +57,28 @@ class LoginScreen extends StatelessWidget {
           children: <Widget>[
             TextField(
               controller: _usernameController,
-              decoration: InputDecoration(labelText: 'Username'),
+              decoration: InputDecoration(labelText: 'identify'),
             ),
             SizedBox(height: 10),
             TextField(
               controller: _passwordController,
-              decoration: InputDecoration(labelText: 'Password'),
-              obscureText: true, // Hide the password text
+              decoration: InputDecoration(labelText: 'password'),
+              obscureText: true,
             ),
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: _handleLogin,
               child: Text('Login'),
             ),
-            SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: _handleRegistration,
-              child: Text('Register'),
-            ),
           ],
         ),
       ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: LoginScreen(),
+  ));
 }
