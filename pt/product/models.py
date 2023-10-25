5f3f904325359a7a5db3c148ae2364f895ab360a
kimkyuhun <yj302kim@naver.com>
2023-10-26 03:52:26 +0900

    good

--- pt/product/models.py
+++ pt/product/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+# Create your models here.
+from django.db import models
+
 class User(models.Model):
     id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=50)  # 사용자명
